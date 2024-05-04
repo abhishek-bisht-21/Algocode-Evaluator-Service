65a2b9d4b8c9ca0f1784cbcadbd5c40e917e0407
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-04 22:35:04 +0530

    Modified queue processor for priority

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const serverConfig_1 = __importDefault(require("./config/serverConfig"));
 const routes_1 = __importDefault(require("./routes"));
+const SampleWorker_1 = __importDefault(require("./workers/SampleWorker"));
+const sampleQueueProducer_1 = __importDefault(require("./producers/sampleQueueProducer"));
 const app = (0, express_1.default)();
 app.use('/api', routes_1.default);
 app.listen(serverConfig_1.default.PORT, () => {
     console.log(`Server started at *:${serverConfig_1.default.PORT}`);
+    (0, SampleWorker_1.default)('SampleQueue');
+    (0, sampleQueueProducer_1.default)('SampleJob', {
+        name: "Abhishek",
+        company: "SOTI",
+        position: "SDET-1",
+        locatiion: "Remote | GGN | Noida"
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,17 @@ import express, { Express } from "express";
 import serverConfig from "./config/serverConfig";
 import apiRouter from "./routes";
 import SampleWorker from "./workers/SampleWorker";
-import sampleQueueProducer from "./producers/sampleQueueProducer";
+import bullBoardAdapter from "./config/bullBoardConfig";
 
 const app: Express = express();
 
 app.use('/api', apiRouter);
+app.use('/ui', bullBoardAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
 	console.log(`Server started at *:${serverConfig.PORT}`);
 
-	SampleWorker('SampleQueue');
+	console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/ui`);
 
-	sampleQueueProducer('SampleJob', {
-		name: "Abhishek",
-		company: "SOTI",
-		position: "SDET-1",
-		locatiion: "Remote | GGN | Noida"
-	});
+	SampleWorker('SampleQueue');
 });  
