68f3a63f506ae5aec34a0348fd23559786ac4abd
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-26 16:58:57 +0530

    Added removing container logic

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_1 = __importDefault(require("express"));
 const serverConfig_1 = __importDefault(require("./config/serverConfig"));
+const runPythonDocker_1 = __importDefault(require("./containers/runPythonDocker"));
 const routes_1 = __importDefault(require("./routes"));
 const SampleWorker_1 = __importDefault(require("./workers/SampleWorker"));
 const bullBoardConfig_1 = __importDefault(require("./config/bullBoardConfig"));
@@ -19,4 +20,13 @@ app.listen(serverConfig_1.default.PORT, () => {
     console.log(`Server started at *:${serverConfig_1.default.PORT}`);
     console.log(`BullBoard dashboard running on: http://localhost:${serverConfig_1.default.PORT}/ui`);
     (0, SampleWorker_1.default)('SampleQueue');
+    const code = `x = input()
+y = input()
+print("value of x is", x)
+print("value of y is", y)
+`;
+    const inputCase = `100
+200
+`;
+    (0, runPythonDocker_1.default)(code, inputCase);
 });
